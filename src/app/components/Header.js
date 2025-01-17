491b94ee98a6691700e98d5d91e83a04c1bb351d
Ali Raza <alirazakhalid1129@gmail.com>
2025-01-17 17:41:59 +0500

    change colors

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="px-3 shadow-sm flex items-center justify-between w-full bg-gray-200 text-gray-900">
-      <div className="py-4 px-8 bg-[#b5ca8d] font-bold">SAMPLE</div>
+      <div className="py-4 px-8 bg-[#ceff1a] text-black font-bold">SAMPLE</div>
       <div className="sm:flex">
         <Link
-          className="mx-1 py-4 px-8 bg-[#2a4849] text-gray-100 hover:bg-[#222e50]"
+          className="mx-1 py-4 px-8 bg-[#6622cc] text-white hover:bg-[#6622ccdd]"
           href="/"
         >
           Home
         </Link>
         <Link
-          className="py-4 px-8 bg-[#2a4849] text-gray-100 hover:bg-[#222e50]"
+          className="py-4 px-8 bg-[#6622cc] text-white hover:bg-[#6622ccdd]"
           href="/posts"
         >
           Posts
