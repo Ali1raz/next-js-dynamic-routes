d244176fd70f1005e4de513d4066d4f71d6ce66e
Ali Raza <alirazakhalid1129@gmail.com>
2025-01-19 10:02:35 +0500

    change colors

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="w-full mt-8 p-3 text-gray-300">
+      Next.js dynamic routes...
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import Posts from "./posts/page";
-
 export default function Home() {
   return (
     <div className=" w-full font-[family-name:var(--font-geist-sans)]">
-      home page
+      <p>This uses express js as backend server.</p>
     </div>
   );
 }
